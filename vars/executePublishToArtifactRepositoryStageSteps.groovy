5fbefbd320beeba52eb5fea1824e98e9aa26cdd9
Brian Mericle <brianm@crosslaketech.com>
2018-05-04 16:57:01 -0500

    skip jacoco during deployment

--- vars/executePublishToArtifactRepositoryStageSteps.groovy
+++ vars/executePublishToArtifactRepositoryStageSteps.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo("Executing [Publish to Artifact Repository] stage steps...")
 
-    sh("mvn -DskipSourceCompile=true -DskipTestCompile=true -Dskip.surefire.tests=true -Dskip.failsafe.tests=true -Dartifact.repo.host=${NEXUS_SERVICE_HOST} -Dartifact.repo.port=${NEXUS_SERVICE_PORT} -e deploy")
+    sh("mvn -Djacoco.skip=true -DskipSourceCompile=true -DskipTestCompile=true -Dskip.surefire.tests=true -Dskip.failsafe.tests=true -Dartifact.repo.host=${NEXUS_SERVICE_HOST} -Dartifact.repo.port=${NEXUS_SERVICE_PORT} -e deploy")
 
     echo("Completed [Publish to Artifact Repository] stage steps.")
 }
