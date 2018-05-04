11b9963f7a905da76da974b3062ba534403d0506
Brian Mericle <brianm@crosslaketech.com>
2018-05-04 11:24:20 -0500

    archive the output files.

--- vars/executeUnitTestsAndStaticAnalysisStagePostSuccessSteps.groovy
+++ vars/executeUnitTestsAndStaticAnalysisStagePostSuccessSteps.groovy
@@ -2,5 +2,6 @@
 
 def call() {
     junit(allowEmptyResults: false, testResults: '**/target/surefire-reports/**/*.xml')
+    archiveArtifacts(artifacts: '*-output.txt')
     echo("Completed [Unit Tests and Static Analysis] stage with result [success].")
 }
