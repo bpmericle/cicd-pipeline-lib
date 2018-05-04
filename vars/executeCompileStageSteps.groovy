1bab2bcf6f19282df7db9251c2baee918ceb60ab
Brian Mericle <brianm@crosslaketech.com>
2018-05-04 12:27:36 -0500

    skip jacoco until needed

--- vars/executeCompileStageSteps.groovy
+++ vars/executeCompileStageSteps.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo("Executing [Compile] stage steps...")
 
-    sh("mvn -e clean test-compile")
+    sh("mvn -e -Djacoco.skip=true clean test-compile")
 
     echo("Completed [Compile] stage steps.")
 }
--- vars/executeUnitTestsStageSteps.groovy
+++ vars/executeUnitTestsStageSteps.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo("Executing [Unit Tests] stage steps...")
 
-    sh("mvn -e -DskipSourceCompile=true -DskipTestCompile=true test")
+    sh("mvn -e -DskipSourceCompile=true -DskipTestCompile=true -Djacoco.skip=true test")
 
     echo("Completed [Unit Tests] stage steps.")
 }
