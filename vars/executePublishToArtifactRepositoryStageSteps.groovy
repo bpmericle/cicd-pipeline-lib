0a3a9c73d5a631aafc6e1864ed30e7534cf9c3d7
Brian Mericle <brianm@crosslaketech.com>
2018-05-04 16:45:41 -0500

    added mvn call to deploy artifact to repo

--- vars/executePublishToArtifactRepositoryStageSteps.groovy
+++ vars/executePublishToArtifactRepositoryStageSteps.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo("Executing [Publish to Artifact Repository] stage steps...")
 
-    //TODO
+    sh("mvn -DskipSourceCompile=true -DskipTestCompile=true -Dskip.surefire.tests=true -Dskip.failsafe.tests=true -e deploy")
 
     echo("Completed [Publish to Artifact Repository] stage steps.")
 }
