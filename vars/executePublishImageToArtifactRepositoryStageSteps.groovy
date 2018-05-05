d96019c8da2d75cbc0a7af986185719e0d637ed1
Brian Mericle <brianm@crosslaketech.com>
2018-05-05 15:48:19 -0500

    update to use https port

--- vars/executePublishImageToArtifactRepositoryStageSteps.groovy
+++ vars/executePublishImageToArtifactRepositoryStageSteps.groovy
@@ -8,13 +8,13 @@ def call() {
     def version = pomInfo.version
     def jarFile = "${artifactId}-${version}.jar"
     def dockerImageTag = "${artifactId}:${version}"
-    def dockerHostAndPort = "${NEXUS_SERVICE_HOST}:${NEXUS_SERVICE_PORT_NEXUS_DOCKER_REPO_PORT}"
+    def dockerHostAndPort = "${NEXUS_SERVICE_HOST}:${NEXUS_SERVICE_PORT_NEXUS_DOCKER_HTTPS_PORT}"
     def dockerRegistryTag = "${dockerHostAndPort}/${dockerImageTag}"
 
     sh("sudo docker build -t latest -t ${dockerImageTag} -t ${dockerRegistryTag} --build-arg JAR_FILE=${jarFile} .")
 
     withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'NEXUS_SERVICE_LOGIN_USERNAME', passwordVariable: 'NEXUS_SERVICE_LOGIN_PASSWORD')]) {
-        sh("sudo docker login -u ${NEXUS_SERVICE_LOGIN_USERNAME} -p ${NEXUS_SERVICE_LOGIN_PASSWORD} ${dockerHostAndPort}")
+        sh("echo ${NEXUS_SERVICE_LOGIN_PASSWORD} | sudo docker login -u ${NEXUS_SERVICE_LOGIN_USERNAME} --password-stdin ${dockerHostAndPort}")
     }
 
     sh("sudo docker push ${dockerRegistryTag}")
