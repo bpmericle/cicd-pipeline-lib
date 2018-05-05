94c96fcc0c8a123e71adde5b69af66f32baa0ad5
Brian Mericle <brianm@crosslaketech.com>
2018-05-04 20:03:50 -0500

    want to see what is printing out

--- vars/executePublishToArtifactRepositoryStageSteps.groovy
+++ vars/executePublishToArtifactRepositoryStageSteps.groovy
@@ -4,8 +4,8 @@ def call() {
     echo("Executing [Publish to Artifact Repository] stage steps...")
 
     withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'NEXUS_SERVICE_LOGIN_USERNAME', passwordVariable: 'NEXUS_SERVICE_LOGIN_PASSWORD')]) {
+        // add back in     set +x    later
         sh '''
-          set +x
           mvn -Djacoco.skip=true -DskipSourceCompile=true -DskipTestCompile=true -Dskip.surefire.tests=true -Dskip.failsafe.tests=true -Dartifact.repo.host=$NEXUS_SERVICE_HOST -Dartifact.repo.port=$NEXUS_SERVICE_PORT -Ddockerfile.username=$NEXUS_SERVICE_LOGIN_USERNAME -Ddockerfile.password=$NEXUS_SERVICE_LOGIN_PASSWORD -s dynamic-settings.xml -e deploy
         '''
     }
