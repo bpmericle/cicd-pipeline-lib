1c759bb7a4f94732e4ec454ba6797e122f8fb8e6
Brian Mericle <brianm@crosslaketech.com>
2018-05-04 19:40:12 -0500

    hide credentials from output

--- vars/executeCompileStageSteps.groovy
+++ vars/executeCompileStageSteps.groovy
@@ -3,42 +3,37 @@
 def call() {
     echo("Executing [Compile] stage steps...")
 
-    def NEXUS_SERVICE_LOGIN_USERNAME = null
-    def NEXUS_SERVICE_LOGIN_PASSWORD = null
     withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        NEXUS_SERVICE_LOGIN_USERNAME = "${USERNAME}"
-        NEXUS_SERVICE_LOGIN_PASSWORD = "${PASSWORD}"
+        // create dynamic settings.xml failsafe
+        def settingsText = """
+  <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 https://maven.apache.org/xsd/settings-1.0.0.xsd">
+    <mirrors>
+      <mirror>
+        <id>crosslake-repo-mirror</id>
+        <name>Crosslake Maven Repository Manager</name>
+        <url>http://${NEXUS_SERVICE_HOST}:${NEXUS_SERVICE_PORT}/repository/crosslake-public/</url>
+        <mirrorOf>*</mirrorOf>
+      </mirror>
+    </mirrors>
+
+    <servers>
+      <server>
+        <id>nexus</id>
+        <username>${NEXUS_SERVICE_LOGIN_USERNAME}</username>
+        <password>${NEXUS_SERVICE_LOGIN_PASSWORD}</password>
+        <configuration>
+          <repository>${NEXUS_SERVICE_HOST}:${NEXUS_SERVICE_PORT}/repository/crosslake-docker/</repository>
+          <tag>latest</tag>
+          <useMavenSettingsForAuth>true</useMavenSettingsForAuth>
+        </configuration>
+      </server>
+    </servers>
+  </settings>
+  """
+        writeFile(file: 'dynamic-settings.xml', text: settingsText)
     }
-
-    // create dynamic settings.xml failsafe
-    def settingsText = """
-<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
-          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-          xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 https://maven.apache.org/xsd/settings-1.0.0.xsd">
-  <mirrors>
-    <mirror>
-      <id>crosslake-repo-mirror</id>
-      <name>Crosslake Maven Repository Manager</name>
-      <url>http://${NEXUS_SERVICE_HOST}:${NEXUS_SERVICE_PORT}/repository/crosslake-public/</url>
-      <mirrorOf>*</mirrorOf>
-    </mirror>
-  </mirrors>
-
-  <servers>
-    <server>
-      <id>nexus</id>
-      <username>${NEXUS_SERVICE_LOGIN_USERNAME}</username>
-      <password>${NEXUS_SERVICE_LOGIN_PASSWORD}</password>
-      <configuration>
-        <repository>${NEXUS_SERVICE_HOST}:${NEXUS_SERVICE_PORT}/repository/crosslake-docker/</repository>
-        <tag>latest</tag>
-        <useMavenSettingsForAuth>true</useMavenSettingsForAuth>
-      </configuration>
-    </server>
-  </servers>
-</settings>
-"""
-    writeFile(file: 'dynamic-settings.xml', text: settingsText)
 
     sh("mvn -Djacoco.skip=true -e -s dynamic-settings.xml clean test-compile")
 
--- vars/executePublishToArtifactRepositoryStageSteps.groovy
+++ vars/executePublishToArtifactRepositoryStageSteps.groovy
@@ -3,14 +3,12 @@
 def call() {
     echo("Executing [Publish to Artifact Repository] stage steps...")
 
-    def NEXUS_SERVICE_LOGIN_USERNAME = null
-    def NEXUS_SERVICE_LOGIN_PASSWORD = null
-    withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        NEXUS_SERVICE_LOGIN_USERNAME = "${USERNAME}"
-        NEXUS_SERVICE_LOGIN_PASSWORD = "${PASSWORD}"
+    withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'NEXUS_SERVICE_LOGIN_USERNAME', passwordVariable: 'NEXUS_SERVICE_LOGIN_PASSWORD')]) {
+        sh '''
+          set +x
+          mvn -Djacoco.skip=true -DskipSourceCompile=true -DskipTestCompile=true -Dskip.surefire.tests=true -Dskip.failsafe.tests=true -Dartifact.repo.host=$NEXUS_SERVICE_HOST -Dartifact.repo.port=$NEXUS_SERVICE_PORT -Ddockerfile.username=$NEXUS_SERVICE_LOGIN_USERNAME -Ddockerfile.password=$NEXUS_SERVICE_LOGIN_PASSWORD -s dynamic-settings.xml -e deploy
+        '''
     }
-
-    sh("mvn -Djacoco.skip=true -DskipSourceCompile=true -DskipTestCompile=true -Dskip.surefire.tests=true -Dskip.failsafe.tests=true -Dartifact.repo.host=${NEXUS_SERVICE_HOST} -Dartifact.repo.port=${NEXUS_SERVICE_PORT} -Ddockerfile.username=${NEXUS_SERVICE_LOGIN_USERNAME} -Ddockerfile.password=${NEXUS_SERVICE_LOGIN_PASSWORD} -s dynamic-settings.xml -e deploy")
 
     echo("Completed [Publish to Artifact Repository] stage steps.")
 }
