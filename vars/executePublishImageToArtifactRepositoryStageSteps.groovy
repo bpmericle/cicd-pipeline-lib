fa4cde383d9bcd3bfb780160192730d38159a51d
Brian Mericle <brianm@crosslaketech.com>
2018-05-05 10:04:29 -0500

    added credentials to the custom docker registry

--- vars/executePublishImageToArtifactRepositoryStageSteps.groovy
+++ vars/executePublishImageToArtifactRepositoryStageSteps.groovy
@@ -6,7 +6,7 @@ def call() {
     def pomInfo = readMavenPom()
 
     withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'NEXUS_SERVICE_LOGIN_USERNAME', passwordVariable: 'NEXUS_SERVICE_LOGIN_PASSWORD')]) {
-        docker.withRegistry("http://${NEXUS_SERVICE_HOST}:${NEXUS_SERVICE_PORT}/repository/crosslake-docker/") {
+        docker.withRegistry("http://${NEXUS_SERVICE_HOST}:${NEXUS_SERVICE_PORT}/repository/crosslake-docker/", 'nexus') {
             def customImage = docker.build("${pomInfo.artifactId}:${pomInfo.version}", "--build-arg JAR_FILE=${pomInfo.build.finalName}.jar")
 
             /* Push the container to the custom Registry */
