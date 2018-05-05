ce43d475b16fec35e5be75be77003588bcea652f
Brian Mericle <brianm@crosslaketech.com>
2018-05-04 19:37:23 -0500

    hide credentials from output

--- vars/executeStaticAnalysisStageSteps.groovy
+++ vars/executeStaticAnalysisStageSteps.groovy
@@ -3,12 +3,12 @@
 def call() {
     echo("Executing [Static Analysis] stage steps...")
 
-    def SONARQUBE_SERVICE_LOGIN_TOKEN = null
-    withCredentials([string(credentialsId: 'sonarqube-token', variable: 'TOKEN')])
-        SONARQUBE_SERVICE_LOGIN_TOKEN = "${TOKEN}"
+    withCredentials([string(credentialsId: 'sonarqube-token', variable: 'TOKEN')]) {
+        sh '''
+          set +x
+          mvn -Dsonar.host.url=http://$SONARQUBE_SERVICE_HOST:$SONARQUBE_SERVICE_PORT -Dsonar.login=$TOKEN -s dynamic-settings.xml -e sonar:sonar
+        '''
     }
-
-    sh("mvn -Dsonar.host.url=http://${SONARQUBE_SERVICE_HOST}:${SONARQUBE_SERVICE_PORT} -Dsonar.login=${SONARQUBE_SERVICE_LOGIN_TOKEN} -s dynamic-settings.xml -e sonar:sonar")
 
     echo("Completed [Static Analysis] stage steps.")
 }
