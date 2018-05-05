f0896e643db025b1a5760a7c54cd59280ee2bff7
Brian Mericle <brianm@crosslaketech.com>
2018-05-05 16:09:43 -0500

    testing

--- vars/executePublishImageToArtifactRepositoryStageSteps.groovy
+++ vars/executePublishImageToArtifactRepositoryStageSteps.groovy
@@ -8,15 +8,16 @@ def call() {
     def version = pomInfo.version
     def jarFile = "${artifactId}-${version}.jar"
     def dockerImageTag = "${artifactId}:${version}"
+    def dockerImageTagLatest = "${artifactId}:latest"
     def dockerHostAndPort = "${NEXUS_SERVICE_HOST}:${NEXUS_SERVICE_PORT_NEXUS_DOCKER_HTTPS_PORT}"
     def dockerRegistryTag = "${dockerHostAndPort}/${dockerImageTag}"
 
-    sh("sudo docker build -t latest -t ${dockerImageTag} -t ${dockerRegistryTag} --build-arg JAR_FILE=${jarFile} .")
-
+    sh("sudo docker build -t ${dockerImageTagLatest} -t ${dockerImageTag} -t ${dockerRegistryTag} --build-arg JAR_FILE=${jarFile} .")
+/*
     withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'NEXUS_SERVICE_LOGIN_USERNAME', passwordVariable: 'NEXUS_SERVICE_LOGIN_PASSWORD')]) {
         sh("echo ${NEXUS_SERVICE_LOGIN_PASSWORD} | sudo docker login -u ${NEXUS_SERVICE_LOGIN_USERNAME} --password-stdin ${dockerHostAndPort}")
     }
-
+*/
     sh("sudo docker push ${dockerRegistryTag}")
 
     echo("Completed [Publish Image] stage steps.")
