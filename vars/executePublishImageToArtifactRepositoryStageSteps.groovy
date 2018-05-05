47da6183464383901df41e3b515fae02ef906692
Brian Mericle <brianm@crosslaketech.com>
2018-05-05 15:34:01 -0500

    added push to docker registry

--- vars/executePublishImageToArtifactRepositoryStageSteps.groovy
+++ vars/executePublishImageToArtifactRepositoryStageSteps.groovy
@@ -7,10 +7,13 @@ def call() {
     def artifactId = pomInfo.artifactId
     def version = pomInfo.version
     def jarFile = "${artifactId}-${version}.jar"
-    def dockerRegistry = "http://${NEXUS_SERVICE_HOST}:${NEXUS_SERVICE_PORT}/repository/crosslake-docker/"
     def dockerImageTag = "${artifactId}:${version}"
+    def dockerRegistry = "http://${NEXUS_SERVICE_HOST}:${NEXUS_SERVICE_PORT_NEXUS_DOCKER_REPO_PORT}/repository/crosslake-docker/"
+    def dockerRegistryTag = "${dockerRegistry}/${dockerImageTag}"
 
-    sh("sudo docker build -t ${dockerImageTag} --build-arg JAR_FILE=${jarFile} .")
+    sh("sudo docker build -t latest -t ${dockerImageTag} -t ${dockerRegistryTag} --build-arg JAR_FILE=${jarFile} .")
+
+    sh("sudo docker push ${dockerRegistryTag}")
 
     echo("Completed [Publish Image] stage steps.")
 }
