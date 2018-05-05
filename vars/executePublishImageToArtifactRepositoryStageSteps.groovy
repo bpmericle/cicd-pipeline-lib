f50d1e6d62630e3afeb89b3e324fda3c458d0bcf
Brian Mericle <brianm@crosslaketech.com>
2018-05-05 10:15:37 -0500

    removed withCredentials b/c the docker registry takes a credentials id

--- vars/executePublishImageToArtifactRepositoryStageSteps.groovy
+++ vars/executePublishImageToArtifactRepositoryStageSteps.groovy
@@ -5,13 +5,11 @@ def call() {
 
     def pomInfo = readMavenPom()
 
-    withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'NEXUS_SERVICE_LOGIN_USERNAME', passwordVariable: 'NEXUS_SERVICE_LOGIN_PASSWORD')]) {
-        docker.withRegistry("http://${NEXUS_SERVICE_HOST}:${NEXUS_SERVICE_PORT}/repository/crosslake-docker/", 'nexus') {
-            def customImage = docker.build("${pomInfo.artifactId}:${pomInfo.version}", "--build-arg JAR_FILE=${pomInfo.artifactId}-${pomInfo.version}.jar")
+    docker.withRegistry("http://${NEXUS_SERVICE_HOST}:${NEXUS_SERVICE_PORT}/repository/crosslake-docker/", 'nexus') {
+        def customImage = docker.build("${pomInfo.artifactId}:${pomInfo.version}", "--build-arg JAR_FILE=${pomInfo.artifactId}-${pomInfo.version}.jar")
 
-            /* Push the container to the custom Registry */
-            customImage.push()
-        }
+        /* Push the container to the custom Registry */
+        customImage.push()
     }
 
     echo("Completed [Publish Image] stage steps.")
