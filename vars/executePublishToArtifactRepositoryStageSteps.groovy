d81c175ff98266032df30d01a3f207af1997c276
Brian Mericle <brianm@crosslaketech.com>
2018-05-05 09:07:17 -0500

    minor refactor

--- vars/executePublishToArtifactRepositoryStageSteps.groovy
+++ vars/executePublishToArtifactRepositoryStageSteps.groovy
@@ -3,12 +3,10 @@
 def call() {
     echo("Executing [Publish to Artifact Repository] stage steps...")
 
+    sh("mvn -Djacoco.skip=true -DskipSourceCompile=true -DskipTestCompile=true -Dskip.surefire.tests=true -Dskip.failsafe.tests=true -Dartifact.repo.host=${NEXUS_SERVICE_HOST} -Dartifact.repo.port=${NEXUS_SERVICE_PORT} -s dynamic-settings.xml -e deploy")
+/*
     withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'NEXUS_SERVICE_LOGIN_USERNAME', passwordVariable: 'NEXUS_SERVICE_LOGIN_PASSWORD')]) {
-        // add back in     set +x    later
-        sh '''
-          mvn -Djacoco.skip=true -DskipSourceCompile=true -DskipTestCompile=true -Dskip.surefire.tests=true -Dskip.failsafe.tests=true -Dartifact.repo.host=$NEXUS_SERVICE_HOST -Dartifact.repo.port=$NEXUS_SERVICE_PORT -Ddockerfile.username=$NEXUS_SERVICE_LOGIN_USERNAME -Ddockerfile.password=$NEXUS_SERVICE_LOGIN_PASSWORD -s dynamic-settings.xml -e deploy
-        '''
     }
-
+*/      
     echo("Completed [Publish to Artifact Repository] stage steps.")
 }
