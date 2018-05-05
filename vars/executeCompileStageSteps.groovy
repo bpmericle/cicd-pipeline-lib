c12e95e9490d73e17707c5b44a4e4e0db129a3f7
Brian Mericle <brianm@crosslaketech.com>
2018-05-05 12:58:37 -0500

    trying running docker as sudo

--- vars/executeCompileStageSteps.groovy
+++ vars/executeCompileStageSteps.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo("Executing [Compile] stage steps...")
 
-    sh("sudo -S docker version")
+    sh("echo \'\' | sudo -S docker version")
 
     withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'NEXUS_SERVICE_LOGIN_USERNAME', passwordVariable: 'NEXUS_SERVICE_LOGIN_PASSWORD')]) {
         // create dynamic settings.xml failsafe
