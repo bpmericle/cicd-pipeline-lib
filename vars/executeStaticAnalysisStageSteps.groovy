b9141fb8f0593bb4af41bc50363b556f2fd0c01a
Brian Mericle <brianm@crosslaketech.com>
2018-05-05 08:45:34 -0500

    testing quality gate detection

--- vars/executeStaticAnalysisStageSteps.groovy
+++ vars/executeStaticAnalysisStageSteps.groovy
@@ -3,20 +3,20 @@
 def call() {
     echo("Executing [Static Analysis] stage steps...")
 
+    /*
     withCredentials([string(credentialsId: 'sonarqube-token', variable: 'TOKEN')]) {
-        sh '''
-          set +x
-          mvn -Dsonar.host.url=http://$SONARQUBE_SERVICE_HOST:$SONARQUBE_SERVICE_PORT -Dsonar.login=$TOKEN -s dynamic-settings.xml -e sonar:sonar
-        '''
+        withSonarQubeEnv('sonarqube') {
+            sh("mvn -Dsonar.host.url=http://$SONARQUBE_SERVICE_HOST:$SONARQUBE_SERVICE_PORT -Dsonar.login=$TOKEN -s dynamic-settings.xml -e sonar:sonar")
+        }
+    }
+    */
+
+    withSonarQubeEnv('sonarqube') {
+        sh("mvn -s dynamic-settings.xml -e sonar:sonar")
     }
 
-    timeout(time: 45, unit: 'SECONDS') { 
-        def qg = waitForQualityGate()
-        if (qg.status != 'OK') {
-            error "Pipeline aborted due to static analysis quality gate failure: ${qg.status}"
-        } else {
-            echo("Static analysis quality gate passed.")
-        }
+    timeout(time: 45, unit: 'SECONDS') {
+        waitForQualityGate(abortPipeline: true)
     }
 
     echo("Completed [Static Analysis] stage steps.")
