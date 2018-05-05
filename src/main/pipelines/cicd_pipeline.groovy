fb695ac7f1d2bc260cabceca16ffe9b2aed2adb9
Brian Mericle <brianm@crosslaketech.com>
2018-05-05 09:18:51 -0500

    moved a file to a new directory

--- src/main/pipelines/cicd_pipeline.groovy
+++ src/main/pipelines/cicd_pipeline.groovy
@@ -1,5 +1,10 @@
 #!/usr/bin/env groovy
 
+folder('pipelines') {
+    displayName("Pipelines")
+    description("The base folder containing pipelines managed by JaC.")
+}
+
 multibranchPipelineJob('pipelines/cicd-app-pipeline') {
     displayName('CICD Application Pipeline')
     description('Declarative Pipeline for the cicd app demo.')
