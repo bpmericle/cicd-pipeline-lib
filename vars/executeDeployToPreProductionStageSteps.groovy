d9b596b66b57460c12d39c51cb4c1488464d6fb2
Brian Mericle <brianm@crosslaketech.com>
2018-05-06 16:04:05 -0500

    testing

--- vars/executeDeployToPreProductionStageSteps.groovy
+++ vars/executeDeployToPreProductionStageSteps.groovy
@@ -3,9 +3,7 @@
 def call() {
     echo("Executing [Deploy to Pre-Production] stage steps...")
 
-    withKubeConfig([credentialsId: 'docker-cloud', serverUrl: "${KUBERNETES_SERVICE_HOST}"]) {
-        sh('kubectl get pods')
-    }
+    sh('kubectl get pods')
 
     echo("Completed [Deploy to Pre-Production] stage steps.")
 }
