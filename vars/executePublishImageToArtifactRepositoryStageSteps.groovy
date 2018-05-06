7647ca199e7e60271d3816103a2812ac21db3b2f
Brian Mericle <brianm@crosslaketech.com>
2018-05-06 13:20:53 -0500

    cleanup

--- vars/executePublishImageToArtifactRepositoryStageSteps.groovy
+++ vars/executePublishImageToArtifactRepositoryStageSteps.groovy
@@ -9,17 +9,18 @@ def call() {
     def jarFile = "${artifactId}-${version}.jar"
     def dockerImageTag = "${artifactId}:${version}"
     def dockerImageTagLatest = "${artifactId}:latest"
-    //def dockerHostAndDockerPort = "${INTERNAL_PROXY_SERVICE_HOST}:${NEXUS_SERVICE_PORT_DOCKER}"
     def dockerHostAndDockerPort = "${NEXUS_HOSTNAME_FOR_DOCKER}:${NEXUS_SERVICE_PORT_DOCKER}"
     def dockerRegistryTag = "${dockerHostAndDockerPort}/${dockerImageTag}"
+    def dockerRegistryTagLatest = "${dockerHostAndDockerPort}/${dockerImageTagLatest}"
 
-    sh("sudo docker build -t ${dockerImageTag} -t ${dockerRegistryTag} -t ${dockerImageTagLatest} --build-arg JAR_FILE=${jarFile} .")
+    sh("sudo docker build -t ${dockerImageTag} -t ${dockerImageTagLatest} -t ${dockerRegistryTag} -t ${dockerRegistryTagLatest} --build-arg JAR_FILE=${jarFile} .")
 
     withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'LOGIN_USERNAME', passwordVariable: 'LOGIN_PASSWORD')]) {
         sh("echo ${LOGIN_PASSWORD} | sudo docker login --username ${LOGIN_USERNAME} --password-stdin ${dockerHostAndDockerPort}")
     }
 
     sh("sudo docker push ${dockerRegistryTag}")
+    sh("sudo docker push ${dockerRegistryTagLatest}")
 
     echo("Completed [Publish Image] stage steps.")
 }
