2bb9113b25d7bf45a937e8d4c826ed4c811d4338
Brian Mericle <brianm@crosslaketech.com>
2018-05-06 19:44:39 -0500

    refactored

--- vars/executeDeployToProductionStageSteps.groovy
+++ vars/executeDeployToProductionStageSteps.groovy
@@ -3,19 +3,8 @@
 def call() {
     echo("Executing [Deploy to Production] stage steps...")
 
-    def pomInfo = readMavenPom()
-    def artifactId = pomInfo.artifactId
-    def version = pomInfo.version
-    def dockerImageTag = "${artifactId}:${version}"
-    def dockerHostAndDockerPort = "${NEXUS_HOSTNAME}:${NEXUS_SERVICE_PORT_DOCKER}"
-    def imageTag = "${dockerHostAndDockerPort}/${dockerImageTag}"
     def namespace = 'production'
-    def templateFileName = 'kubernetesfile.yml'
-    def replacementValues = [:]
-    replacementValues['artifactId'] = artifactId
-    replacementValues['imageTag'] = imageTag
-
-    kubernetes.apply(namespace, templateFileName, replacementValues)
+    kubernetes.apply(namespace)
 
     echo("Completed [Deploy to Production] stage steps.")
 }
--- vars/kubernetes.groovy
+++ vars/kubernetes.groovy
@@ -1,12 +1,17 @@
 #!/usr/bin/env groovy
 
-def apply(namespace, templateFileName, replacementValues) {
-    def appConfigFileTemplate = readFile(file: templateFileName)
-    def appConfigFileContent = appConfigFileTemplate.replaceAll('@@ARTIFACT_ID@@', replacementValues['artifactId'])
-    appConfigFileContent = appConfigFileContent.replaceAll('@@IMAGE_TAG@@', replacementValues['imageTag'])
-    appConfigFileContent = appConfigFileContent.replaceAll('@@NAMESPACE@@', namespace)
-
+def apply(namespace, templateFileName='kubernetes-app-config-template.yml') {
+    def pomInfo = readMavenPom()
+    def artifactId = pomInfo.artifactId
+    def version = pomInfo.version
+    def dockerImageTag = "${artifactId}:${version}"
+    def dockerHostAndDockerPort = "${NEXUS_HOSTNAME}:${NEXUS_SERVICE_PORT_DOCKER}"
+    def imageTag = "${dockerHostAndDockerPort}/${dockerImageTag}"
     def appConfigFileName = "${namespace}-config.yml"
+
+    def appConfigFileTemplate = readFile(file: templateFileName)
+    def appConfigFileContent = appConfigFileTemplate.replaceAll('@@ARTIFACT_ID@@', artifactId).replaceAll('@@IMAGE_TAG@@', imageTag).replaceAll('@@NAMESPACE@@', namespace)
+
     writeFile(file: appConfigFileName, text: appConfigFileContent)
 
     sh("kubectl apply -f ${appConfigFileName}")
