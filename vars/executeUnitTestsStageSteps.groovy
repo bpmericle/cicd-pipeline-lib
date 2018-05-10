000f0c3c1efaa68d4a747338f73cea17ced279e6
Brian Mericle <brianm@crosslaketech.com>
2018-05-09 18:22:07 -0700

    teting static analysis

--- vars/executeUnitTestsStageSteps.groovy
+++ vars/executeUnitTestsStageSteps.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo("Executing [Unit Tests] stage steps...")
 
-    sh("mvn -DskipSourceCompile=true -DskipTestCompile=true -Dskip.failsafe.tests=true -Dmaven.javadoc.skip=true -s dynamic-settings.xml -e test")
+    sh("mvn -DskipSourceCompile=true -DskipTestCompile=true -Dskip.failsafe.tests=true -Dmaven.javadoc.skip=true -s dynamic-settings.xml -e verify")
 
     echo("Completed [Unit Tests] stage steps.")
 }
