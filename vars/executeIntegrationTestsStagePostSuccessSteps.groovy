ec1837bdaba05c2259b8c3db85b64f9b8967f27c
Brian Mericle <>
2018-06-30 11:42:44 -0500

    updates

--- vars/executeIntegrationTestsStagePostSuccessSteps.groovy
+++ vars/executeIntegrationTestsStagePostSuccessSteps.groovy
@@ -1,6 +1,8 @@
 #!/usr/bin/env groovy
 
 def call() {
-    junit(allowEmptyResults: false, testResults: '**/target/failsafe-reports/**/*.xml')
+    if ('false' == ${SKIP}) {
+        junit(allowEmptyResults: false, testResults: '**/target/failsafe-reports/**/*.xml')
+    }
     echo("Completed [Integration Tests] stage with result [success].")
 }
--- vars/executeUnitTestsStagePostSuccessSteps.groovy
+++ vars/executeUnitTestsStagePostSuccessSteps.groovy
@@ -1,6 +1,8 @@
 #!/usr/bin/env groovy
 
 def call() {
-    junit(allowEmptyResults: false, testResults: '**/target/surefire-reports/**/*.xml')
+    if ('false' == ${SKIP}) {
+        junit(allowEmptyResults: false, testResults: '**/target/surefire-reports/**/*.xml')
+    }
     echo("Completed [Unit Tests] stage with result [success].")
 }
