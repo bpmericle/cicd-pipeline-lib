0f2f6c38cdce3fd7601086acdd35bf02a609b89c
Brian Mericle <>
2018-06-30 11:50:05 -0500

    removed skip from unit tests

--- vars/executeUnitTestsStageSteps.groovy
+++ vars/executeUnitTestsStageSteps.groovy
@@ -3,9 +3,7 @@
 def call() {
     echo("Executing [Unit Tests] stage steps...")
 
-    if ('false' == SKIP) {
-        sh("mvn -DskipSourceCompile=true -DskipTestCompile=true -Dskip.failsafe.tests=true -Dmaven.javadoc.skip=true -s dynamic-settings.xml -e verify")
-    }
+    sh("mvn -DskipSourceCompile=true -DskipTestCompile=true -Dskip.failsafe.tests=true -Dmaven.javadoc.skip=true -s dynamic-settings.xml -e verify")
 
     echo("Completed [Unit Tests] stage steps.")
 }
